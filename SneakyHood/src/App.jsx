39925af62e042d4cb24748dde3b1513be862bbbb
Dhruv nirwan <dhruvnirwan11@gmail.com>
2026-07-28 17:40:47 +0530

    Products collections cards are ready with props and json

--- SneakyHood/src/App.jsx
+++ SneakyHood/src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
 import ProductCard from './components/ProductCard';
+import Products from "./Data/Products.json"
+import './app.css'
 function App() {
+
 
   return (
     <>
+
     <Navbar></Navbar>
+    <section className='collections'>{Products.map((product)=>(
+        <ProductCard key={product.id} name={product.name} rating={product.rating} price={product.price} discount={product.discount}/>
+    ))}</section>
+      
     </>
     
     
--- SneakyHood/src/components/ProductCard.jsx
+++ SneakyHood/src/components/ProductCard.jsx
@@ -1,75 +1,38 @@
+
+import { Strikethrough } from "lucide-react";
 import "./ProductCard.css";
-import {
-  Heart,
-  Star,
-  ShoppingCart,
-} from "lucide-react";
 
-function ProductCard() {
+export default function ProductCard({name,price,discount,rating}) {
+  let discounted_price = (price -(discount*price/100)).toFixed(2)
+  
+
   return (
+    <>
     <div className="product-card">
+      <div>{discount===0?(<span></span>):<div  className="product-badge">{discount}% OFF</div>}</div>
 
-      <div className="product-image">
-
-        <span className="category-badge">
-          New
-        </span>
-
-        <button className="wishlist-btn">
-          <Heart size={20} />
-        </button>
-
+      <div className="product-image-box">
         <img
-          src="SneakyHood\src\assets\Images\nike slipper.jpg"
-          alt="Sneaker"
+          src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=600&auto=format&fit=crop"
+          alt="Nike shoe"
+          className="product-image"
         />
-
       </div>
 
-      <div className="product-content">
+      <div className="product-name-container">{name} </div>
 
-        <p className="product-brand">
-          SneakyHood
-        </p>
+      <div className="product-meta">
+        <span className="product-price">{discount===0?(<span>${price}</span>):(<> <strike className="strikethrough_price">
+                  <span>${price}</span>
+                </strike>
+                <span className="discounted-price">${discounted_price}</span>
+              </>)}</span>
 
-        <h3>
-          Air Phantom X
-        </h3>
-
-        <div className="rating">
-
-          <Star
-            size={16}
-            fill="#FFD700"
-            color="#FFD700"
-          />
-
-          <span>4.8</span>
-
-        </div>
-
-        <div className="price-row">
-
-          <div>
-            <span className="price">
-              $129
-            </span>
-
-            <span className="old-price">
-              $169
-            </span>
-          </div>
-
-          <button className="cart-btn">
-            <ShoppingCart size={18} />
-          </button>
-
-        </div>
-
+        <span className="product-rating">{rating} ★</span>
       </div>
 
+      <button className="product-btn">View Details</button>
     </div>
+    </>
   );
 }
-
-export default ProductCard;
